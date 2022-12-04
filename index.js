df337a4967a24bbe19f280de6ef6e41d6f8c9885
Tate <tatebraeckel@gmail.com>
2022-12-04 11:41:59 -0700

    commit tree 061cc2fca149e240eaccaa8143ccc8a9ec55f2fb

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+console.log(`I'm ${age} years old. Happy birthday to me!`);
+
+}
+
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+
+    }
+ 
+   return gifts;
+}
+
+wrapGifts(gifts); 
+
+
+ names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names) {
+     let messages = [];
+     names.forEach((name,index) => {
+messages.push("Thank you, " + name + ", for the wonderful surprise gift!");
+
+     })
+     return messages;
+
+}
+
+console.log(writeCards(names));
+
+
+
+
+function countDown() {
+    let count = 0;
+    while (count < 11) {
+
+        console.log(count);
+        count += 1;
+       
+    }
+    return count;
+}
+    
+
